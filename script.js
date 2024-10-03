b0bc7f0a0ea905dd3b947bcd4ddb541185f9365b
Hardik Pratap <hardikpratap188@gmail.com>
2024-10-03 22:54:01 +0530

    done respnsiveness

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    function toggleMenu() {
+        const menu = document.querySelector('.list'); // Selecting the navbar list
+        menu.classList.toggle('open'); // Toggle the 'open' class
+    }
